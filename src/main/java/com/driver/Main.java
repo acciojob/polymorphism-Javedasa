2f3079cc30b17a0a20b4194ce5cf36cf506ff701
Javed Ansari <ansarizawed4@gmail.com>
2023-09-16 21:54:00 +0530

    added code to main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,23 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+        Product p=new Product();
+        p.product(10,20);
+        p.product(20,34,45);
+        p.product(2.0,4.0) ;
+    }
+
+     static class Product{
+     public int product(int x,int y){
+           return x*y;
+     }
+     public int product(int x,int y,int z){
+         return x*y*z;
+     }
+     public double product(double x, double y) {
+              return x*y;
+          }
+      }
+      //comment
 }
