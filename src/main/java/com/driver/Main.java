873b1a15ce7ce86ad159446e41147b91278d19bd
Javed Ansari <ansarizawed4@gmail.com>
2023-09-17 13:10:57 +0530

    final commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,17 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 
     public static void main(String[] args){
         Product p=new Product();
@@ -10,14 +21,4 @@ public class Main {
     }
 
 }
-class Product{
-    public int product(int x,int y){
-        return x*y;
-    }
-    public int product(int x,int y,int z){
-        return x*y*z;
-    }
-    public double product(double x, double y) {
-        return x*y;
-    }
-}
+
