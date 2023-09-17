4f02b00deb0c012884be7b1041fee801623b8e2a
Javed Ansari <ansarizawed4@gmail.com>
2023-09-17 12:38:47 +0530

    added final code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,16 +9,15 @@ public class Main {
         p.product(2.0,4.0) ;
     }
 
-     static class Product{
-     public int product(int x,int y){
-           return x*y;
-     }
-     public int product(int x,int y,int z){
-         return x*y*z;
-     }
-     public double product(double x, double y) {
-              return x*y;
-          }
-      }
-      
+}
+class Product{
+    public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double product(double x, double y) {
+        return x*y;
+    }
 }
